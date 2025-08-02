03c53ce3bb16b1563fb034fe1bbdf3685c418194
Gustavo Pereira <gsp.gustavo2010s@gmail.com>
2025-08-02 08:49:20 -0400

    Delete polybar/old.polybar directory

--- polybar/old.polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-polybar-msg cmd quit
-
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bottom 2>&1 | tee -a /tmp/polybar2.log & disown
-
-echo "Bars launched..."
